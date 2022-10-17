7f83ac30bd523227ad1a36a7e01b599f2d7ae466
Md Irbaaz Hussain <94373243+Irbaaz49@users.noreply.github.com>
2022-10-17 19:17:06 +0530

    Update WorkData.js

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -4,6 +4,46 @@ export const Work = [
    
     {
         id:1,
+        name:"E-Com App (Amazon Clone)",
+        description:"Its a Amazon clone, with add to cart, signin/singup functionality",
+        tags:[ "React", "Firebase", "Material UI", "Context api"],
+        
+        demo:"https://e-com-app-eight.vercel.app/",
+        github:"https://github.com/Irbaaz49/E-Com-App"
+        
+    },
+    {
+        id:2,
+        name:"Ultimate React Admin Dashboard",
+        description:"This project includes a fully functional calendar, a kanban board (drag and drop elements), a WYSIWYG editor and much more..",
+        tags:["React", "Tailwind CSS", "Syncfusion"],
+        
+        demo:"https://dashboard-orpin-zeta.vercel.app/",
+        github:"https://github.com/Irbaaz49/dashboard-"
+        
+    },
+    {
+        id:3,
+        name:"Service Offering website",
+        description:"It is a clone of the Loriku website and is used for cloud hosting and the deployment of all types of web apps, from massive enterprise APIs to small-scale static webpages for individuals.",
+        tags:["HTML","CSS", "JavaScript"],
+        
+        demo:"https://singular-platypus-8e0765.netlify.app/",
+        github:"https://github.com/Irbaaz49/HTML-CSS-Project---HTML-CSS-Mini-Project---sh874ce71if6"
+        
+    },
+    {
+        id:4,
+        name:"Movie Searching Website",
+        description:"Website that allows you to search for movies and TV episodes starring your favorite stars",
+        tags:["React","HTML","CSS", "JS"],
+
+        
+        demo:"https://github.com/Irbaaz49/Post-man-clone",
+        github:"https://github.com/Irbaaz49/Post-man-clone"
+       
+    },{
+        id:5,
         name:"Tic-Tac-Toe game",
         description:"Simple game ,Play with bot",
         tags:["js","html","css"],
@@ -12,60 +52,21 @@ export const Work = [
         github:"https://github.com/Irbaaz49/Tic-Tac-Toe-game"
         
     },
-    {
-        id:2,
-        name:"Weather App",
-        description:" Enable users to get instant alerts regarding weather conditions using weather api",
-        tags:["html","css", "js", "api"],
-        
-        demo:"https://1p65o.csb.app/",
-        github:"https://github.com/Irbaaz49/Weather-App"
-        
-    },
-    {
-        id:3,
-        name:"Radio Player",
-        description:"Radio player using api and other react components",
-        tags:["react","html","css"],
-        
-        demo:"https://csb-cu8pu.netlify.app/",
-        github:"https://github.com/Irbaaz49/Radio"
-        
-    },
-    {
-        id:4,
-        name:"Post man clone",
-        description:"Postman is a popular application programming interface that makes it easy for developers to create, share, test and document APIs. ",
-        tags:["html","css"," vanilla js"],
-        
-        demo:"https://github.com/Irbaaz49/Post-man-clone",
-        github:"https://github.com/Irbaaz49/Post-man-clone"
-       
-    },{
-        id:5,
-        name:"Portfolio - website",
-        description:"Simple parallax portfolio website",
-        tags:["react","js","css"],
-        
-        demo:"https://qe2ck.csb.app/",
-        github:"https://github.com/Irbaaz49/portfolio"
-        
-    },
    
     {
         id:6,
-        name:"car-game ",
-        description:"A simple car game made with vanilla javascript",
-        tags:["html","css"," vanilla js"],
+        name:"Ping Pong Game",
+        description:"A cool Ping Pong game made with vanilla javascript with  multiplayer capability and the dual-mode",
+        tags:["HTML Canvas","CSS"," Vanilla Js"],
         
-        demo:"https://github.com/Irbaaz49/car-game",
-        github:"https://github.com/Irbaaz49/car-game"
+        demo:"https://cheerful-salmiakki-24156b.netlify.app/",
+        github:"https://github.com/Irbaaz49/JS-Mini-Project-1-Ping-Pong-Game-"
     },
     {
         id:7,
-        name:"React News-App",
-        description:"News App using react hooks , api",
-        tags:["react","html","css"],
+        name:"YouTube Comment Section",
+        description:"Comments area where user can add a comment, reply to that comment and can delete that comment.",
+        tags:["HTML","CSS","JS"],
         
         demo:"https://github.com/Irbaaz49/react-News-App-",
         github:"https://github.com/Irbaaz49/react-News-App-"
@@ -75,19 +76,19 @@ export const Work = [
         id:8,
         name:"Texttutil",
         description:"Copy and paste your text into the editor to count its words and characters, to clear extra spaces, to upper case or lower etc.",
-        tags:["react","html","css"],
+        tags:["React","HTML","CSS"],
         
         demo:"https://irbaaz49.github.io/Texttutil/",
         github:"https://github.com/Irbaaz49/Texttutil"
         
     },
     {
-        id:8,
+        id:9,
         name:"React Instagram clone",
-        description:"It is build on top of the React JS",
-        tags:["react","html","css","js"],
+        description:"Static Instagram home page, It is build on top of the React JS",
+        tags:["React","HTML","CSS"],
         
-        demo:"https://github.com/Irbaaz49/Instagram-front-end-clone-in-react",
+        demo:"https://stupefied-montalcini-d58f2e.netlify.app/",
         github:"https://github.com/Irbaaz49/Instagram-front-end-clone-in-react"
     },
     
